29dc6784f026c8516c71c2502d83f6c86faaac07
Itaru Kitagawa <kitagawa@bizreach.co.jp>
2015-02-24 10:11:14 +0900

    Option17thFeb

--- simple/src/main/scala/com/github/tanacasino/study/kitaly/random/Option17thFeb.scala
+++ simple/src/main/scala/com/github/tanacasino/study/kitaly/random/Option17thFeb.scala
@@ -9,22 +9,27 @@ object Option17thFeb {
 
   def main(args: Array[String]) {
 
-    val x3: Option[String] = Some("")
+    // Input Data
+    val x3: Option[String] = Some("abc")
     val x5: Option[String] = Some("123")
     
+    
+    // Validation
     val either3 = x3.filter(_.nonEmpty).toRight(("x3", "error.required"))
-    val either5 = Try( 
-      x5.filter(_.nonEmpty).map(_.toInt)
+    
+    val either5 = Try(
+      x5.withFilter(_.nonEmpty).map(_.toInt)
     ).toOption.flatten.toRight(("x5", "error.number"))
 
+    // Merge validation errors into a map
+    val errMap = List(either3, either5)
+      .withFilter(_.isLeft) map {
+        case Left((key, value)) => (key, value)
+      } toMap
     
-    println(either3.toString)
-    println(either5.toString)
-    
-    val errMap = List(either3, either5).filter(_.isLeft) map {
-      case Left((key, value)) => (key, value)
-    } toMap
-    
+    // Print out results
+    println(either3)
+    println(either5)
     println(errMap)
   }
 
