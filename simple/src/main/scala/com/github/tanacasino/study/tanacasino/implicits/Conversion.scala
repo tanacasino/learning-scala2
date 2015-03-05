02bbe1a05668ee3cb40d84555e234bcb0e1fd1bd
saito400 <ken.s.530000@gmail.com>
2015-03-06 07:15:17 +0900

    daily scala 0305

--- simple/src/main/scala/com/github/tanacasino/study/tanacasino/implicits/Conversion.scala
+++ simple/src/main/scala/com/github/tanacasino/study/tanacasino/implicits/Conversion.scala
@@ -6,10 +6,17 @@ object Conversion {
   implicit class RichDateTime(dt: DateTime) { 
     def +(period: Period): DateTime = dt.plus(period)
     def -(period: Period): DateTime = dt.minus(period)
+    def nextDay: DateTime = dt.plus(1.day)
+    def nextMonth: DateTime = dt.plus(1.month)
   }
 
   implicit class RichPeriod(i: Int) {
     def day: Period = Period.days(i)
+    def minute: Period = Period.minutes(i)
+    def hour: Period = Period.hours(i)
+    def month: Period = Period.months(i)
+    def nextDay: Period = Period.months(1)
+
   }
 
   def main(args: Array[String]) {
@@ -18,8 +25,11 @@ object Conversion {
     println(now)
     val tomorrow = now + 1.day
     println(tomorrow)
+    println(now - 1.day)
+    println(now - 1.minute)
+    println(now - 6.hour)
+    println(now.nextDay)
 
   }
-
 
 }
