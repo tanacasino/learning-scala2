7eb4e52da0f48f3c1c2f2650715db113b183efee
unknown <shintaro.tamaki@BIZ2014-ODDR010.bizreach.co.jp>
2015-03-06 10:13:09 +0900

    DaylyScala implicit conversion編 java.timeでやってみました。

---
+++ simple/src/main/scala/com/github/tanacasino/study/tamaki/ConversionForJavaTime.scala
@@ -0,0 +1,41 @@
+package com.github.tanacasino.study.tamaki
+
+import java.time._
+
+/**
+ * Created by shintaro.tamaki on 2015/03/06.
+ */
+object ConversionForJavaTime {
+
+  // java.timeでやってみました。
+  implicit class RichDateTime(dt: LocalDateTime) {
+    def +(period: Period): LocalDateTime = dt.plus(period)
+    def -(period: Period): LocalDateTime = dt.minus(period)
+    def +(duration: Duration): LocalDateTime = dt.plus(duration)
+    def -(duration: Duration): LocalDateTime = dt.minus(duration)
+    def nextDay: LocalDateTime = dt.plus(1.day)
+    def nextMonth: LocalDateTime = dt.plus(1.month)
+  }
+
+  implicit class RichDuration(i: Int) {
+    def minute: Duration = Duration.ofMinutes(i)
+    def hour: Duration = Duration.ofHours(i)
+  }
+
+  implicit class RichPeriod(i: Int) {
+    def day: Period = Period.ofDays(i)
+    def month: Period = Period.ofMonths(i)
+    def nextDay: Period = Period.ofDays(1)
+  }
+
+  def main(args: Array[String]) {
+    val now = LocalDateTime.now()
+    println(now)
+    val tomorrow = now + 1.day
+    println(tomorrow)
+    println(now - 1.day)
+    println(now - 1.minute)
+    println(now - 6.hour)
+    println(now.nextDay)
+  }
+}
